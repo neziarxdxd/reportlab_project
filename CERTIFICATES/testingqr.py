50719be476a19d436db9b323080d70a416a27f18
raizengfilosofuxd <rickdavidsangalang@gmail.com>
2019-11-04 10:39:30 +0800

    done

--- CERTIFICATES/testingqr.py
+++ CERTIFICATES/testingqr.py
@@ -1,23 +1,36 @@
 from reportlab.pdfgen import canvas
 from reportlab.graphics.shapes import Drawing 
 from reportlab.graphics.barcode.qr import QrCodeWidget 
-from reportlab.graphics import renderPDF
+from reportlab.graphics import renderPDF,shapes
+
+
+
+
 from reportlab.lib.pagesizes import letter
 from reportlab.lib.pagesizes import landscape
 
-p = canvas.Canvas("sample22.pdf", pagesize=landscape(letter))
-data = 'Helo World!'
-qrw = QrCodeWidget(data) 
+p = canvas.Canvas("sample25.pdf", pagesize=landscape(letter))
 
-b = qrw.getBounds()
+collect = ['Raizen','John','Isaac','Jacob','Newton', 'Gosling', 'Gates']
+x = 0
+y= 0
+for data in collect:
+    qrw = QrCodeWidget(data) 
 
-w=b[2]-b[0] 
-h=b[3]-b[1] 
-p.drawString(160,20,data)
-d = Drawing(200,100,transform=[330./w,0,0,330./h,0,0]) 
-d.add(qrw)
+    b = qrw.getBounds()
 
-renderPDF.draw(d, p, 10, 10)
+    w=b[2]-b[0] 
+    h=b[3]-b[1] 
+    
+    d = Drawing(200,100,transform=[100./w,0,0,100./h,0,0]) 
+    d.add(shapes.String(45,0,data, textAnchor="middle"))
+
+    d.add(qrw)
+    
+    renderPDF.draw(d, p,10, 10+y)
+    y+=100
+    x+=30
+
 
 
 p.showPage()
