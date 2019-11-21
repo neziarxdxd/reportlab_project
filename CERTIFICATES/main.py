00dde801c429eda9f3adb38cd27c604d53cff37f
raizengfilosofuxd <rickdavidsangalang@gmail.com>
2019-11-21 16:07:08 +0800

    added

--- CERTIFICATES/main.py
+++ CERTIFICATES/main.py
@@ -3,7 +3,9 @@ from reportlab.lib.pagesizes import letter
 from reportlab.lib.pagesizes import landscape
 
 c= canvas.Canvas("sample22.pdf", pagesize=landscape(letter))
-c.setFont('Helvetica',30)
+c.setFont('Helvetica',20)
 c.drawCentredString(415,500, "Certificate of Completion")
+c.setFont('Helvetica',50)
+c.drawCentredString(415,400, "RAIZEN SANGALANG",)
 c.showPage()
 c.save()
