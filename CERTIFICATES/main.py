c0df828504478fc3ca0dbd57290053abcbe9b71c
raizengfilosofuxd <rickdavidsangalang@gmail.com>
2019-11-22 22:41:51 +0800

    done

--- CERTIFICATES/main.py
+++ CERTIFICATES/main.py
@@ -11,13 +11,12 @@ from reportlab.lib.pagesizes import landscape
 
 pdfmetrics.registerFont(TTFont('Rale', 'Raleway-Bold.ttf'))
 c= canvas.Canvas("sample23.pdf", pagesize=landscape(letter))
-c.drawImage("image.png", 415,200, width=350,height=275,mask=None) 
+c.drawImage("image.png", 350,200, width=350,height=275,mask=None) 
 c.setFont('Rale',20)
 c.setFillColor(HexColor(0xff8100))
 c.drawCentredString(415,500, "Certificate of Completion")
 c.setFont('Helvetica',50)
-
 c.setFillColor(HexColor(0xf5d100))
-c.drawCentredString(415,400, "RAIZEN SANGALANG",)
+c.drawCentredString(415,400, "RAIZENG SANGALAN RENN")
 c.showPage()
 c.save()
